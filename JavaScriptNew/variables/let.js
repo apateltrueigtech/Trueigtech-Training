81f9a8cef75b7120e9f07079a897fb3fa5b7db10
apateltrueigtech <apateltrueigtech.com>
2025-03-12 17:31:55 +0530

    variables

---
+++ JavaScriptNew/variables/let.js
@@ -0,0 +1,17 @@
+// let a=12;
+// let b= "Hello";
+// console.log(a);
+// console.log(b);
+
+function myFunction(){
+   let x=10;
+   console.log(x);
+if(true){
+    let y=20;
+    console.log(y);
+}
+console.log(y);
+console.log(x);
+}
+myFunction();
+ console.log(x);
---
+++ JavaScriptNew/variables/var.js
@@ -0,0 +1,12 @@
+var a= "Hello";
+var b=10;
+console.log(a);
+console.log(b);
+
+
+function greet(){
+    var message ="Hello World";
+    console.log(message);
+}
+greet();
+console.log(message);

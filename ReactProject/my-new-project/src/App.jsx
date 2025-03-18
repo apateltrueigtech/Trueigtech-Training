f6827277e4a8b02b4ad1045608af888648bc4eb2
apateltrueigtech <apateltrueigtech.com>
2025-03-18 08:17:56 +0530

    counterapp

--- ReactProject/my-new-project/src/App.jsx
+++ ReactProject/my-new-project/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react';
+
 import './App.css'
 import Header from './components/Header';
 import Student from './components/Student';
+import Counter from './components/counter';
 
 function App() {
 
 
   
-    
-    
-      <Header/>
-        const [count, setCount] = useState(0)
-        return (
-          <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-          <h1 className="text-blue-500 text-3xl font-bold mb-4">Counter App</h1>
-          <div className="flex items-center gap-4 bg-white p-6 rounded-lg shadow-lg">
-            <button 
-              className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
-              onClick={() => setCount(count - 1)}
-            >
-              DECREMENT
-            </button>
-            <h2 className="text-2xl font-semibold">{count}</h2>            
-            <button 
-              className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
-              onClick={() => setCount(count + 1)}
-            >
-              INCREMENT
-            </button>
-          </div>
-        </div>
+    return(
+    <div>
+        <Header/>
+      <Counter/>
+    </div>
+       
+    )
         
         
       
@@ -39,7 +24,7 @@ function App() {
       // <Student name="Sachin" age={26} isStudent={false}/>
       // <Student name="sandy"/> */}
      
-  )
+  
 }
 
 export default App
